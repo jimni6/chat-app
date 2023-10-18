18b2515d6b120d54296f0f58addd568a853ca104
NiJimmy <nijimmy@outlook.fr>
2023-10-18 14:35:36 +0200

    User can view a register page on app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { Register } from "./pages/Register";
 
 function App() {
   return (
     <div>
-
+      <Register />
     </div>
   );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Register.css'
+import Add from '../assets/addImg.svg'
+
+export const Register = () => {
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">MaChat'</span>
+                <h1 className="title">Création d'un nouveau compte</h1>
+                <form className="formRegister">
+                    <input type="text" placeholder="Nom d'utilisateur" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Mot de passe" />
+                    <input style={{display: "none"}}type="file" id="file"/>
+                    <label className="addAvatarContainer" htmlFor="file">
+                        <img className="addImg" src={Add} alt=''/>
+                        <span className="spanAvatar" >Ajouter une image</span>
+                    </label>
+                    <button className='formButton'>C'est parti!</button>
+                </form>
+                <p>Vous avez déjà un compte? S'identifier</p>
+            </div>
+        </div>
+    )
+}
